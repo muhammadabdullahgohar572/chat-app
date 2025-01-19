2dd95a9ad7039de5bd00c374f27ac9f3a1c5dd3a
Abdullah <muhammadabdullahgohar572@gmail.com>
2025-01-19 05:30:15 +0500

    first commit

--- components/ChatForms.js
+++ components/ChatForms.js
@@ -13,55 +13,62 @@ import {
 
 import "stream-chat-react/dist/css/v2/index.css";
 
+export const ChatForms = ({ slug, clerkUser }) => {
+  function capitalize(str) {
+    if (!str) return ""; // Handle empty or undefined input
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+  const apiKey = "bxfuxpqtdst7";
+  const userId = clerkUser.id;
+  const userName = clerkUser.name;
+  const userToken = clerkUser.token;
 
-export const ChatForms=({slug,clerkUser})=>{
-    function capitalize(str) {
-        if (!str) return ""; // Handle empty or undefined input
-        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-      }
-    const apiKey = 'bxfuxpqtdst7';
-    const userId = clerkUser.id;
-    const userName = clerkUser.name;
-    const userToken = clerkUser.token;
-    
-    const user = {
-        id: userId,
-        name: userName,
-        image: `https://getstream.io/random_png/?name=${userName}`,
-      };
+  console.warn({
+    slug: slug,
+    clerkUser: clerkUser,
+    userId: userId,
+    userName: userName,
+    userToken: userToken,
+  });
 
-      const [channel, setChannel] = useState();
-      const client = useCreateChatClient({
-        apiKey,
-        tokenOrProvider: userToken,
-        userData: user,
-      });
-    
-      useEffect(() => {
-        if (!client) return;
-    
-        const channel = client.channel('messaging', slug, {
-          image: 'https://getstream.io/random_png/?name=react',
-          name:capitalize(slug)+" Chat",
-          members: [userId],
-        });
-    
-        setChannel(channel);
-        channel.addMembers([userId]);
-      }, [client]);
-    
-      if (!client) return <div>Setting up client & connection...</div>;
-    
-      return (
-        <Chat client={client}>
-          <Channel channel={channel}>
-            <Window>
-              <ChannelHeader />
-              <MessageList />
-              <MessageInput />
-            </Window>
-            <Thread />
-          </Channel>
-        </Chat>
-      );
-}
+  const user = {
+    id: userId,
+    name: userName,
+    image: `https://getstream.io/random_png/?name=${userName}`,
+  };
+
+  const [channel, setChannel] = useState();
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: user,
+  });
+
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel("messaging", slug, {
+      image: "https://getstream.io/random_png/?name=react",
+      name: capitalize(slug) + " Chat",
+      members: [userId],
+    });
+
+    setChannel(channel);
+    channel.addMembers([userId]);
+  }, [client]);
+
+  if (!client) return <div>Setting up client & connection...</div>;
+
+  return (
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
